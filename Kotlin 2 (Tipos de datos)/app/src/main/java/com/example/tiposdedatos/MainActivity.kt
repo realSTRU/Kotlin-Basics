7cc68738e8ff1a4192ced5c6a52c913b888cd429
Cesar Reynoso <cesarreynoso0969@gmail.com>
2023-04-21 11:44:07 -0400

    Data Types: Decimals (Float, Double)

--- Kotlin 2 (Tipos de datos)/app/src/main/java/com/example/tiposdedatos/MainActivity.kt
+++ Kotlin 2 (Tipos de datos)/app/src/main/java/com/example/tiposdedatos/MainActivity.kt
@@ -29,6 +29,17 @@ class MainActivity : AppCompatActivity() {
 
         println("El numero resultante de la suma de " + byInt +" mas "+ byInt2 +" es: "+  byInt3)
 
+        //Decimals (Float, Double)
+
+        val byDouble :Double = 2.10
+        val byDouble1 : Double = 3.7
+        val byDouble2 : Double = 8.9
+
+        val byDouble3 : Double = byDouble + byDouble1 + byDouble2
+
+        println("El resultado de sumar los numero decimales en kotlin de los numeros ("+byDouble+" "+byDouble1+" "+byDouble2+") es: "+byDouble3)
+
+
     }
 }
 
